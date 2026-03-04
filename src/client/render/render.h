e8fd83a2ef667d3ea54cb9bf532615410f193b90
plunyo <conradthieu87@gmail.com>
2026-03-04 12:38:08 +1100

    A

--- src/client/render/render.h
+++
@@ -1,8 +0,0 @@
-#ifndef RENDER_H
-#define RENDER_H
-
-void InitRender(void);
-void DrawRender(void);
-void CloseRender(void);
-
-#endif
--- src/server/server.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <enet/enet.h>
-
-int RunServer();
-void UpdateServer(ENetHost* server, ENetEvent* event);
